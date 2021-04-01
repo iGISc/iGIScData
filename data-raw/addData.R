8ff366723ec38c53a98895b2949d0a2d40b26d44
spedunkler <jerry@sfsu.edu>
2021-03-31 18:30:58 -0700

    Update addData.R

--- data-raw/addData.R
+++ data-raw/addData.R
@@ -8,48 +8,48 @@
 library(tidyverse)
 overwrite = TRUE
 
-sierraFeb <- read_csv("data-raw/sierraFeb.csv")
+sierraFeb <- read_csv("inst/extdata/sierraFeb.csv")
 usethis::use_data(sierraFeb)
 
-sierraClimate <- read_csv("data-raw/sierraClimate.csv")
+sierraClimate <- read_csv("inst/extdata/sierraClimate.csv")
 usethis::use_data(sierraClimate, overwrite=TRUE)
 
-ConcentrationReport <- read_csv("data-raw/ConcentrationReport.csv")
+ConcentrationReport <- read_csv("inst/extdata/ConcentrationReport.csv")
 usethis::use_data(ConcentrationReport)
 
-sierraData <- read_csv("data-raw/sierraData.csv")
+sierraData <- read_csv("inst/extdata/sierraData.csv")
 usethis::use_data(sierraData)
 
-sierraStations<- read_csv("data-raw/sierraStations.csv")
+sierraStations<- read_csv("inst/extdata/sierraStations.csv")
 usethis::use_data(sierraStations)
 
-soilCO2_97<- read_csv("data-raw/soilCO2_97.csv") %>%
+soilCO2_97<- read_csv("inst/extdata/soilCO2_97.csv") %>%
   rename(`SOIL C`, `TEMPC`) %>%
   select(SITE, DATE, TEMPC, `CO2%`)
 usethis::use_data(soilCO2_97)
 
-tidy_eucoak<- read_csv("data-raw/tidy_eucoak.csv")
+tidy_eucoak<- read_csv("inst/extdata/tidy_eucoak.csv")
 usethis::use_data(tidy_eucoak)
 
-XSptsNDVI<- read_csv("data-raw/XSptsNDVI.csv")
+XSptsNDVI<- read_csv("inst/extdata/XSptsNDVI.csv")
 usethis::use_data(XSptsNDVI)
 
-eucoakrainfallrunoffTDR<- read_csv("data-raw/eucoakrainfallrunoffTDR.csv")
+eucoakrainfallrunoffTDR<- read_csv("inst/extdata/eucoakrainfallrunoffTDR.csv")
 usethis::use_data(eucoakrainfallrunoffTDR)
 
-CA_ClimateNormals <- read_csv("data-raw/CA_ClimateNormals.csv")
+CA_ClimateNormals <- read_csv("inst/extdata/CA_ClimateNormals.csv")
 usethis::use_data(CA_ClimateNormals)
 
-TRI_1987_CA <- read_csv("data-raw/TRI_1987_CA.csv")
+TRI_1987_CA <- read_csv("inst/extdata/TRI_1987_CA.csv")
 usethis::use_data(TRI_1987_CA)
 
-TRI_1997_CA <- read_csv("data-raw/TRI_1997_CA.csv")
+TRI_1997_CA <- read_csv("inst/extdata/TRI_1997_CA.csv")
 usethis::use_data(TRI_1997_CA)
 
-TRI_2007_CA <- read_csv("data-raw/TRI_2007_CA.csv")
+TRI_2007_CA <- read_csv("inst/extdata/TRI_2007_CA.csv")
 usethis::use_data(TRI_2007_CA)
 
-TRI_2017_CA <- read_csv("data-raw/TRI_2017_CA.csv")
+TRI_2017_CA <- read_csv("inst/extdata/TRI_2017_CA.csv")
 usethis::use_data(TRI_2017_CA)
 
 
@@ -58,15 +58,15 @@ library(sf)
 library(raster)
 
 
-CA_counties <- st_read("data-raw/CA_counties.shp")
+CA_counties <- st_read("inst/extdata/CA_counties.shp")
 usethis::use_data(CA_counties)
 
-BayAreaCounties <- st_read("data-raw/BayAreaCounties.shp")
+BayAreaCounties <- st_read("inst/extdata/BayAreaCounties.shp")
 usethis::use_data(BayAreaCounties)
 
-BayAreaTracts <- st_read("data-raw/BayAreaTracts.shp")
+BayAreaTracts <- st_read("inst/extdata/BayAreaTracts.shp")
 usethis::use_data(BayAreaTracts)
 
-CAfreeways <- st_read("data-raw/CAfreeways.shp")
+CAfreeways <- st_read("inst/extdata/CAfreeways.shp")
 usethis::use_data(CAfreeways)
 
